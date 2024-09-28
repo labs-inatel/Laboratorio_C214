a88154fd1b4ef05d124cd78a8d212d807a00273a
Iza Lopes <102091381+Izalp@users.noreply.github.com>
2024-09-28 09:06:11 -0300

    :wrench: chore: add Vercel Analytics integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { LocalStorageAdapter } from "./LocalStorageAdapter.ts";
+import { Analytics } from "@vercel/analytics/react"
 
 const localStorageAdapter = new LocalStorageAdapter();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <App toDoListRepository={localStorageAdapter} />
   </StrictMode>
 );
